1dc503df6802d6cb36a3134cc92fdb1bc77e42b7
Ben Houston <ben@exocortex.com>
2015-10-06 14:56:00 -0400

    no roughness for glass.

--- spec/bsdfs/glass/glass.js
+++ spec/bsdfs/glass/glass.js
@@ -1,5 +1,5 @@
 // TODO, add Abbe number support
-var microfacetDistribution = new TrowbridgeReitzDistribution( roughness, anisotropy, anisotropyRotation, anisotropyTangent );
+var microfacetDistribution = new TrowbridgeReitzDistribution( 0, 0, 0, null );
 var fresnel = new FresnelDielectric( 1, ior );
 bsdfs.add( new MicrofacetReflection( reflectance, microfacetDistribution, fresnel ) )
 bsdfs.add( new MicrofacetTransmission( transmittance, microfacetDistribution, 1, ior ) )
