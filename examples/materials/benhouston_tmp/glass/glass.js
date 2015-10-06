b184ad5ed4ba95a85c8edfeaf06d62d2d0fa9a6c
Ben Houston <ben@exocortex.com>
2015-10-06 16:19:27 -0400

    more mdl, fix conflict.

--- examples/materials/benhouston_tmp/glass/glass.js
+++
@@ -1,5 +0,0 @@
-// TODO, add Abbe number support
-var microfacetDistribution = new TrowbridgeReitzDistribution( 0, 0, 0, null );
-var fresnel = new FresnelDielectric( 1, ior );
-bsdfs.add( new MicrofacetReflection( reflectance, microfacetDistribution, fresnel ) )
-bsdfs.add( new MicrofacetTransmission( transmittance, microfacetDistribution, 1, ior ) )
--- examples/materials/benhouston_tmp/metal/metal.js
+++
@@ -1,3 +0,0 @@
-var microfacetDistribution = GetDistribution( roughnessModel, roughness, anisotropy, anisotropyRotation, anisotropyTangent );
-var fresnel = new FresnelConductor( 1, ior, kappa );
-bsdfs.add( new MicrofacetReflection( 1, microfacetDistribution, fresnel ) );
