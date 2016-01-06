b4fd26b518d54126f45d1daeab3bf42df70ffae3
Ben Houston <ben@exocortex.com>
2016-01-06 13:10:31 -0500

    more work.

--- test/main.js
+++ test/main.js
@@ -1,35 +1,44 @@
 var fs = require( 'fs' );
+var fa = require( 'fa' );
 var path = require( 'path' );
 var OMG = require('../lib');
 var R = require('ramda');
 
 var resourcesDirectory = "/resources";
 
+var specLibrary = new OMG.SpecLibrary();
+
 var currentDirectory = path.resolve(__dirname + resourcesDirectory );
-var specDirectory = path.resolve( (__dirname + resourcesDirectory + "/spec" );
+var specDirectory = path.resolve( __dirname + resourcesDirectory + "/spec" );
 
 //var specProvider = new OMG.SpecProvider();
 //specProvider.path.push( specDirectory );
 
+var specUsageCount = {};
 //if( fs.exists)
 console.log( 'currentDirectory', currentDirectory );
 fs.readdir( currentDirectory, function( err, files ) {
 	console.log( 'err', err );
 	console.log( 'files', files );
 
-	var loadVRMat = file => {
+	var loadVRMat = ( file, callback ) => {
 		var fullPath = path.resolve( currentDirectory + '/' + file );
-		console.log( 'fullPath', fullPath );
+		console.log( 'reading: ', fullPath );
 		OMG.VRMat.parseFromFile( fullPath, vrMat => {
 			//console.log( 'vrMat', vrMat );
 			var outputPath = path.resolve(__dirname + resourcesDirectory + "/output.json" );
 			//console.log( 'outputPath', outputPath );
 
 			VRMat.specCreator( vrMat, function( err, name, data ) {
-				fs.writeFile( name + ".json", JSON.stringify( data, null, 4 ) );
-			} );
-			fs.writeFile( outputPath,  JSON.stringify( vrMat, null, '  ' ) );
+				specLibrary.add( data );
+				specUsageCount[ name ] = ( specUsageCount[ name ] || 0 ) + 1;
+			});
+			fs.writeFile( outputPath,  JSON.stringify( vrMat, null, '  ' ), callback );
 		} );
 	};
-	R.forEach( loadVRMat, files );
+
+	fa.each(files, loadVRMat, function(err) {
+		console.log( specUsageCount );
+	});
+
 });
